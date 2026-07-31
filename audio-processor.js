64fde87020aa762cd546190f80ab9d6ce19b6518
ajshooting <135501897+ajshooting@users.noreply.github.com>
2026-07-31 17:03:43 +0900

    Downmix captured stereo audio to mono

--- audio-processor.js
+++ audio-processor.js
@@ -1,13 +1,24 @@
 class AudioProcessor extends AudioWorkletProcessor {
   process(inputs, outputs, parameters) {
-    // We only need the first channel of the first input.
-    const inputChannel = inputs[0][0];
-
-    // Post the audio data back to the main thread (popup.js).
-    // The data is sent as a Float32Array.
-    if (inputChannel) {
-      this.port.postMessage(inputChannel);
+    const inputChannels = inputs[0];
+    if (!inputChannels || inputChannels.length === 0) {
+      return true;
     }
+
+    const leftChannel = inputChannels[0];
+    const rightChannel = inputChannels[1];
+    const monoChannel = new Float32Array(leftChannel.length);
+
+    if (rightChannel) {
+      for (let i = 0; i < leftChannel.length; i++) {
+        monoChannel[i] = (leftChannel[i] + rightChannel[i]) * 0.5;
+      }
+    } else {
+      monoChannel.set(leftChannel);
+    }
+
+    // Post the mono audio data back to the main thread (popup.js).
+    this.port.postMessage(monoChannel, [monoChannel.buffer]);
 
     // Keep the processor alive.
     return true;
